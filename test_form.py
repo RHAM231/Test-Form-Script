98462f7a1ccbb80926509e4028e76f230fc39bea
Rex Mitchell <nogardjmj@gmail.com>
2022-02-01 20:44:16 -0600

    Setting up code, getting html classes for code

--- test_form.py
+++ test_form.py
@@ -1,5 +1,4 @@
-from selenium import webdriver
-import pandas as pd
+# Python Base Imports
 import time
 import json
 from datetime import datetime
@@ -7,70 +6,81 @@ import pathlib
 import glob
 import sys
 sys.path.append(".")
-import faker
-import numpy as np
-import pandas as pd
+
+# Installed Packages
+from selenium import webdriver
+# Import Service to resolve executable_path deprecation issue
+from selenium.webdriver.chrome.service import Service
+# import pandas as pd
+# import faker
+# import numpy as np
+
 
 
 # https://towardsdatascience.com/automating-submission-forms-with-python-94459353b03e
 
 
-f = faker.Faker()
-colors = ["Blue","Pink","Black","White","Green"]
+##############################################################################
+# BEGIN SCRIPT
+##############################################################################
 
 
-names = [f.name() for _ in range(100)]
-ages =  [np.random.randint(18,65) for _ in range(100)]
-color_choices = [np.random.choice(colors,1)[0] for _ in range(100)]
+s = Service("./chromedriver")
+driver = webdriver.Chrome(service=s)
+print(driver)
 
 
-database = pd.DataFrame(dict(names=names, ages=ages, colors = color_choices))
-database.to_csv("submission_form_database.csv", index=False)
-database.head()
+text_question_element_class = \
+    "quantumWizTextinputPaperinputInput"
+checkbox_question_element_class = \
+    "appsMaterialWizToggleRadiogroupOffRadio"
+submit_element_class = \
+    '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span/span'
 
-text_question_element_class = "quantumWizTextinputPaperinputInput"
-checkbox_question_element_class = "appsMaterialWizToggleRadiogroupOffRadio"
-submit_element_class = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span/span'
+
+# def answerNameAge(driver, df, element_class, user_id):
+#     name = df["names"][user_id]
+#     age = df["ages"][user_id]
+#     text_answers = [name, str(age)] # following the order in the form
+#     text_questions = driver.find_elements_by_class_name(element_class)
+#     for a,q in zip(text_answers,text_questions):
+#         q.send_keys(a)
+#     return driver
 
 
-def answerNameAge(driver, df, element_class, user_id):
-    name = df["names"][user_id]
-    age = df["ages"][user_id]
-    text_answers = [name, str(age)] # following the order in the form
-    text_questions = driver.find_elements_by_class_name(element_class)
-    for a,q in zip(text_answers,text_questions):
-        q.send_keys(a)
+# def answerCheckBox(driver, df, element_class, user_id):
+#     color_answer = df["colors"][user_id]
+#     color_answer_index = color_index_dict[color_answer]
+#     driver.find_elements_by_class_name(
+#         element_class)[color_answer_index].click()
+#     return driver
+
+
+# def submit(driver, element_class):
+#     driver.find_element_by_xpath(element_class).click()
+#     return driver
+
+
+# df = pd.read_csv("./submission_form_database.csv")
+# text_question_element_class = "quantumWizTextinputPaperinputInput"
+# checkbox_question_element_class = "appsMaterialWizToggleRadiogroupOffRadio"
+
+# url = "https://forms.gle/WY7E9N8wkiMtziTD9"
+# driver = webdriver.Chrome(executable_path="./chromedriver")
+# for user_id in range(len(df)):
+#     driver.get(url)
     
-    return driver
+#     driver.maximize_window()
+#     driver = answerNameAge(driver, df, text_question_element_class, user_id)
+#     driver = answerCheckBox(
+#         driver, 
+#         df, 
+#         checkbox_question_element_class, 
+#         user_id
+#         )
+#     driver = submit(driver, submit_element_class)
 
 
-color_index_dict = {"Blue": 0, "Pink": 1, "Black": 2, "White": 3, "Green": 4}
-
-def answerCheckBox(driver, df, element_class, user_id):
-    color_answer = df["colors"][user_id]
-    color_answer_index = color_index_dict[color_answer]
-    driver.find_elements_by_class_name(element_class)[color_answer_index].click()
-    
-    
-    return driver
-
-
-def submit(driver, element_class):
-    driver.find_element_by_xpath(element_class).click()
-    return driver
-
-
-df = pd.read_csv("./submission_form_database.csv")
-text_question_element_class = "quantumWizTextinputPaperinputInput"
-checkbox_question_element_class = "appsMaterialWizToggleRadiogroupOffRadio"
-
-url = "https://forms.gle/WY7E9N8wkiMtziTD9"
-driver = webdriver.Chrome(executable_path="./chromedriver")
-for user_id in range(len(df)):
-    driver.get(url)
-    
-    driver.maximize_window()
-    driver = answerNameAge(driver, df, text_question_element_class, user_id)
-    driver = answerCheckBox(driver, df, checkbox_question_element_class, user_id)
-    driver = submit(driver, submit_element_class)
-
+##############################################################################
+# END
+##############################################################################
