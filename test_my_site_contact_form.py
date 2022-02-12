d567684ae8090fd0453d0ff4251850802791a213
Rex Mitchell <nogardjmj@gmail.com>
2022-02-11 18:37:24 -0600

    Adding comments

--- test_my_site_contact_form.py
+++ test_my_site_contact_form.py
@@ -5,10 +5,27 @@
 
 # Grab our test data and our test functionality from our main test
 # script file.
-
 from test_form import mySiteData, TestDriver
+
+
+##############################################################################
+# BACKGROUND
+##############################################################################
+
+
+# Let's create a Python file for testing just my site's contact
+# form. We'll import our functionality from our main test script file
+# and run it from this file, giving it my site's test data for our
+# contact form. This allows us to have different "buttons" to test
+# different pieces of our three sites seperately. We don't necessarily
+# want to test all three sites at the same frequency.
 
 
 testDriver = TestDriver()
 testDriver.getURL(mySiteData)
 testDriver.test_live_contact_form(mySiteData)
+
+
+##############################################################################
+# END
+##############################################################################
