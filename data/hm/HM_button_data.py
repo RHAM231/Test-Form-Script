6765ecb407de3354d928f3784b368509fd6c24a8
Rex Mitchell <nogardjmj@gmail.com>
2022-04-27 20:22:20 -0500

    Adding HM data

---
+++ data/hm/HM_button_data.py
@@ -0,0 +1,33 @@
+##############################################################################
+# NO IMPORTS, BEGIN SCRIPTS
+##############################################################################
+
+
+# THIS FILE IS IN DEVELOPMENT AND DOES NOT REPRESENT A FINISHED PRODUCT
+
+class HMSiteButtonData(object):
+    def __init__(self):
+        self.urls = {
+                    'home': '', 
+                    '': '',
+                    'contact': '',
+                    'about': ''
+                    }
+        self.ids = {
+                    'home': [
+                        '',
+                        ],
+                    '': [
+                        '',
+                        ],
+                    'contact': [
+                        'id_name', 'id_sender', 'id_subject', 'id_message', 
+                        'id_cc_myself', 'submit-btn-id'
+                        ],
+                    'about': ['']
+                    }
+
+
+##############################################################################
+# END
+##############################################################################
